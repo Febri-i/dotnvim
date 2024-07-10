288db84e3357b04af383d82342800305e4e96e4f
Febri Bayu Nurcahyo <febribayun@gmail.com>
2024-07-10 13:48:08 +0700

    Actually, fuck folding.

--- lua/configs/autocmmand.lua
+++ lua/configs/autocmmand.lua
@@ -1,13 +1,1 @@
-vim.api.nvim_create_autocmd("BufWinLeave", {
-	callback = function()
-		vim.cmd [[mkview!]]
-	end,
-	pattern = "*.*"
-})
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-	callback = function(aa)
-		vim.cmd [[silent loadview]]
-	end,
-	pattern = "*.*"
-})
